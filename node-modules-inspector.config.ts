a6785fe900904e065089d808e527ca106131ea79
Chris <chrisbreuer93@gmail.com>
2025-02-18 08:55:20 -0800

    chore: minor updates

--- node-modules-inspector.config.ts
+++ node-modules-inspector.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'node-modules-inspector'
 
 export default defineConfig({
+  name: '@stacksjs/everything',
+  defaultFilters: {
+    'source-type': 'prod',
+    'exclude-workspace': true,
+  },
+  defaultSettings: {
+    showPublishTimeBadge: true,
+    showInstallSizeBadge: true,
+    showFileComposition: true,
+  },
   excludeDependenciesOf: [
     'eslint',
     'webpack',
-    'node-modules-inspector',
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint/utils',
+  ],
+  excludePackages: [
+    'typescript',
   ],
 })
