3b2909875b82730a73633d7a2ff31fddc96755d3
Chris <chrisbreuer93@gmail.com>
2025-03-13 10:06:59 -0700

    chore: initial commit
    chore: wip
    
    chore: wip
    
    chore: wip

---
+++ README.md
@@ -0,0 +1,17 @@
+# @stacksjs/everything
+
+Every actively maintained npm package by the Stacks community.
+
+It visualizes the dependencies of the packages with [node-module-inspector](https://github.com/antfu/node-modules-inspector). You can find the recent deployed versions on [`stacks-everything.netlify.app`](https://stacks-everything.netlify.app/).
+
+## Sponsors
+
+<p align="center">
+  <a href="https://cdn.jsdelivr.net/gh/stacksjs/static/sponsors.svg">
+    <img src='https://cdn.jsdelivr.net/gh/stacksjs/static/sponsors.svg'/>
+  </a>
+</p>
+
+## License
+
+[MIT](./LICENSE) License © [Stacks.js](https://github.com/stacksjs)
---
+++ node-modules-inspector.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'node-modules-inspector'
+
+export default defineConfig({
+  name: '@antfu/everything',
+  defaultFilters: {
+    sourceType: 'prod',
+    excludeWorkspace: true,
+  },
+  defaultSettings: {
+    showPublishTimeBadge: true,
+    showInstallSizeBadge: true,
+    showFileComposition: true,
+  },
+  excludeDependenciesOf: [
+    'eslint',
+    'webpack',
+    'jsdom',
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint/utils',
+    '@vercel/nft',
+  ],
+  excludePackages: [
+    'typescript',
+    'pnpm',
+    'netlify-plugin-cache',
+    'lint-staged',
+    'simple-git-hooks',
+  ],
+  publint: true,
+})
